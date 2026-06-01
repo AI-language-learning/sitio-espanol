ab3d01b2dc6fe52ac7f361a7b6ae346d15976191
AI-language-learning <nihongo.japones.para.todos@gmail.com>
2026-06-01 17:36:50 +0900

    changes

--- sinopsis_files/sinopsis.js
+++
@@ -1,9 +0,0 @@
-// Created by iWeb 3.0.3 local-build-20110623
-
-setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
-{return false;}
-function onPageLoad()
-{loadMozillaCSS('sinopsis_files/sinopsisMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');Widget.onload();fixAllIEPNGs('Media/transparent.gif');IMpreload('sinopsis_files','shapeimage_3','0');IMpreload('sinopsis_files','shapeimage_3','1');performPostEffectsFixups()}
-function onPageUnload()
-{Widget.onunload();}
--- sinopsis_old_files/sinopsis_old.js
+++
@@ -1,9 +0,0 @@
-// Created by iWeb 3.0.3 local-build-20110623
-
-setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
-{return false;}
-function onPageLoad()
-{loadMozillaCSS('sinopsis_old_files/sinopsis_oldMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');Widget.onload();fixAllIEPNGs('Media/transparent.gif');IMpreload('sinopsis_old_files','shapeimage_2','0');IMpreload('sinopsis_old_files','shapeimage_2','1');performPostEffectsFixups()}
-function onPageUnload()
-{Widget.onunload();}
